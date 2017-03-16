0489e1345b0da318d9a85573353b563f132fd7ab
Vladimir Ganich <wescost@ukr.net>
2017-03-16 12:00:36 +0200

    tests added

---
+++ DjangoWebProject1/SeleniumTests/SeleniumTests.py
@@ -0,0 +1,16 @@
+from selenium import webdriver
+from selenium.webdriver.support.ui import WebDriverWait
+import unittest
+
+class SeleniumTests(unittest.TestCase):
+    """description of class"""
+
+    def setUp(self):
+        self.driver = webdriver.Firefox()
+        self.driver.get("https://www.ukr.net")
+
+    def test1(self):
+        driver=self.driver
+
+    def tearDown(self):
+        self.driver.quit()   
