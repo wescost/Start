5af40840d65df0c5f89d8abf764e19c03059e27e
Vladimir <ITUNIVER10@ITUNIVER10PC>
2017-04-05 20:59:45 +0300

    ghgh

--- DjangoWebProject1/WebProject2/OOP.py
+++ DjangoWebProject1/WebProject2/OOP.py
@@ -24,3 +24,29 @@ def super_main():
     Point1.f(d)
 
 super_main()
+
+class Car:
+    def __init__(self, make, petrol_consumption):
+        self.make = make
+        self.petrol_consumption = petrol_consumption
+
+    def petrol_calculation(self, price = 22.5):
+        return self.petrol_consumption * price
+
+
+ford = Car("ford", 10)
+
+money = ford.petrol_calculation()
+print(money)
+
+class Square:
+    def __init__(self, x):
+        self.x = x
+
+    def find_square(self):
+        return self.x ** 2
+
+s = Square(5)
+print(s.x)
+square = s.find_square()
+print(square)
